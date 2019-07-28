678e625768f28f21de17383ec22582b8179880fd
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-07-28 11:52:33 +0200

    Initial project setup.

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index() => View("MakeBooking", new Appointment { Date = DateTime.Now });
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt) => View("Completed", appt);
+    }
+}
